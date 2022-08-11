35345b0e140cd54817ba0b1f5b96844d597f02f7
o0WhiteTiger0o <supermanrobinhood1986@gmail.com>
2022-08-11 13:09:55 -0500

    no2

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-document.querySelector('h1').textContent='test';
+/*
+---------------------------DATA-------------------
+*/
+const iframe_Link = {
+    page1 :'name'
+}
+/* end data*/
+
+const getSection = document.querySelector('section');
+
+function create_page_func(src){
+    const create_div = document.createElement('div');
+    const create_h1 = document.createElement('h1');
+    getSection.appendChild(create_div);
+    getSection.appendChild(create_h1);
+    const add_text = create_h1.textContent(src);
+    return add_text;
+
+}
+
+create_page_func('ta la zai');
