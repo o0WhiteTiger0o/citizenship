1c4d0787089bc8b71b1d9465bd81d81b8ae33d66
o0WhiteTiger0o <supermanrobinhood1986@gmail.com>
2023-02-04 15:41:58 -0600

    updates 2/4/2023

--- js/index.js
+++ js/index.js
@@ -18,3 +18,25 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+function toggleIframe(index) {
+  var iframes = [document.getElementById("myIframe1"), document.getElementById("myIframe2")];
+  var buttons = [document.getElementById("showBtn1"), document.getElementById("showBtn2")];
+  var iframe = document.getElementById("myIframe" + index);
+  var button = document.getElementById("showBtn" + index);
+  
+  for(var i = 0; i < iframes.length; i++) {
+    if(iframes[i] !== iframe) {
+      iframes[i].style.display = "none";
+      iframes[i].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+    }
+  }
+  
+  if(iframe.style.display === "none") {
+    iframe.style.display = "block";
+    iframe.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+  } else {
+    iframe.style.display = "none";
+    iframe.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+  }
+}
